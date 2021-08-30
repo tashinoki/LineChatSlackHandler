f2bd40b988c9041d4a73cdf4b338cdb5d36ad9bb
tashinoki <kenya.112163@gmail.com>
2021-08-30 22:19:23 +0900

    add slack file message model

--- LineChatSlackHandler/Models/SlackMessage.cs
+++ LineChatSlackHandler/Models/SlackMessage.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+using System.IO;
 
 namespace LineChatSlackHandler.Models
 {
@@ -9,5 +10,13 @@ namespace LineChatSlackHandler.Models
         public string Channel { get; set; }
 
         public string Text { get; set; }
+    }
+
+    [JsonObject(NamingStrategyType = typeof(SnakeCaseNamingStrategy))]
+    public class SlackFileMessage
+    {
+        public string Channel { get; set; }
+
+        public Stream File { get; set; }
     }
 }
